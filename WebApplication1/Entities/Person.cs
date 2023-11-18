091ea711c9f061774641a72c9233550185127283
Ercument <ercukalkann@gmail.con>
2023-11-19 02:29:34 +0300

    person data model, db connection and first migration

---
+++ WebApplication1/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Entities
+{
+    public class Person
+    {
+        public Guid GUID { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public int Age { get; set; } = 0;
+        public int TotalMoney { get; set; } = 0;
+
+    }
+}
